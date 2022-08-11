c314c7b8d080708beb796fffe1c41524a21d6829
Marshel <marshelnetta164@gmail.com>
2022-08-11 08:45:59 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "this is much cool to show in heroku"
 
 
 if __name__=="__main__":
